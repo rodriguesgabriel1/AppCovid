6eb45f67b64c9f76b25d12d81212cc0e789ae25a
Carlos Zarzar <carlos_zarzar@outlook.com>
2023-11-03 16:32:41 -0300

    Modificacao para dashboardshiny e insindo uplodfile no UI

--- app.R
+++ app.R
@@ -1,51 +1,40 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+## app.R ##
 library(shiny)
+library(shinydashboard)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+ui <- dashboardPage(
+  # Cabeçalho
+  dashboardHeader(
+    title = "AppCovid Check"
+  ),
+  # Barra lateral
+  dashboardSidebar(),
+  # Corpo do App
+  dashboardBody(
+    # 1 Linha 
+    fluidRow(
+      # 1 Box
+      box( # importando arquivo .fasta
+        fileInput("file", "Escolha um arquivo .fasta:",
+                  multiple = FALSE,
+                  accept = c(".fasta"))
+      )
+      #-----------
+      
     )
+    
+    
+    
+  )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  # Lendo os arquivos do upload
+  uploaded_data <- reactive({
+    req(input$file) # REQUERIDO Garante que o usuário tenha carregado um arquivo
+    read.csv(input$file$datapath) # Lê o arquivo CSV carregado pelo usuário
+  })
+  
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
