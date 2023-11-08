904da244fe07c84ffdd6ba713c826cac2d0a8058
DPS500 <daniel.pis@discente.ufopa.edu.br>
2023-11-08 20:16:14 -0300

    Atualização do script app para o aplicativo AppCovid, especificamente no painél da dev (Desenvolvedores do aplicativo)

--- app.R
+++ app.R
@@ -2,7 +2,10 @@
 library(shiny)
 library(shinydashboard)
 
+
+
 ui <- dashboardPage(
+  skin = "purple", 
   # Cabeçalho
   dashboardHeader(
     title = "AppCovid Check"
@@ -12,42 +15,312 @@ ui <- dashboardPage(
     sidebarMenu(
       menuItem("Home", tabName = "home"),
       menuItem("Help", tabName = "help"),
-      menuItem("Desenvolvedores", tabName = "Dev"),
+      menuItem("Desenvolvedores", tabName = "Dev",icon = icon("landmark")),
       menuItem("Contato", tabName = "Contact")
     )
   ),
   # Corpo do App
   dashboardBody(
+        fluidRow(
     #Colocar vários menus 
     tabItems(
-      #Primeiro menu HOME
-      tabItem(
-        #Nome do menu desejado
-        tabName = "home",
-          # 1 Box
-          box( # importando arquivo .fasta
-            fileInput("file", "Escolha um arquivo .fasta:",
-                      multiple = FALSE,
-                      accept = c(".fasta"))
-          )
-          #-----------
-          
-        ),
       # segundo menu HELP
       tabItem(
-        tabName = "help"
-      )
-      )
-  )
+        tabName = "Dev",
+        
+        #Título para os Desenvolvedores
+        h1("Desenvolvedores", style= "font-family: 'Times New Roman';
+           font-size: 45pt; text-align: center;font-weight: bold;"),
+        
+        
+        #----------------
+        # 1º Box para a desenvolvedora Denise
+        box(status = "primary",
+            solidHeader = TRUE,
+            width = 6,
+          #--------------
+          #Tags para colocar a imagem de Denise em formato circular
+          tags$img(src="denise.jpeg", class = "img-circle",
+                   width = "200px", height = "200px"),style = "text-align: center",
+          
+          #Título com o nome da desenvolvedora
+          h1(style="font-family: 'Times New Roman'; font-size: 20pt; 
+             text-align: center; font-weight: bold;",
+             "Denise de Assis Paiva"),
+          
+          #Usado para criar um espaço entre o Título e a descrição
+          br(),
+          
+          #Descrição
+          p(style="font-family: 'Times New Roman'; font-size: 15pt; 
+             text-align: center;",
+            "Universidade Federal de Lavras - UFLA"),
+          
+          #Usado para criar um espaço entre o Título e a descrição
+          br(),
+          
+          #Link com um determinado nome
+          a("Currículo Lattes", href= "http://lattes.cnpq.br/6114238098801542",
+            style="font-family: 'Times New Roman'; font-size: 15pt; 
+             text-align: center;")
+        ),
+        
+        
+        
+        
+        #------------------
+        #2º Box para a desenvolvedora Thelma
+        box(
+          status = "primary",
+          solidHeader = TRUE,
+          width = 6,
+          tags$img(src="thelma.jpeg", class = "img-circle",
+                   width = "200px", height = "200px"),
+          
+          #Usado para centralizar o que estiver dentro do box
+          style = "text-align: center",
+          
+          #Título com o nome da desenvolvedora
+          h1(style="font-family: 'Times New Roman'; font-size: 20pt; 
+             text-align: center; font-weight: bold;",
+             "Thelma Sáfadi"),
+          
+          #Usado para criar um espaço entre o Título e a descrição
+          br(),
+          
+          #Descrição
+          p(style= "font-family: 'Times New Roman'; font-size: 15pt;
+            text-align: center;",
+            "Universidade Federal de Lavras - UFLA"),
+          
+          #Usado para criar um espaço entre o Título e a descrição
+          br(),
+          
+          #Link com um determinado nome
+          a("Currículo Lattes", href= "http://lattes.cnpq.br/9821585244827807",
+            style="font-family: 'Times New Roman'; font-size: 15pt; 
+             text-align: center;")
+        ),
+        
+        
+        
+        
+        #------------------
+        #3º Box para a desenvolvedora karla
+        box(
+          status = "primary",
+          solidHeader = TRUE,
+          width = 6,
+          tags$img(src="Karla.jpeg", class = "img-circle",
+                   width = "200px", height = "200px"),
+          
+          #Usado para centralizar o que estiver dentro do box
+          style = "text-align: center",
+          
+          #Título com o nome da desenvolvedora
+          h1(style="font-family: 'Times New Roman'; font-size: 20pt; 
+             text-align: center; font-weight: bold;",
+             "Karla Suemy Clemente Yotoko"),
+          
+          #Usado para criar um espaço entre o Título e a descrição
+          br(),
+          
+          #Descrição
+          p(style= "font-family: 'Times New Roman'; font-size: 15pt; 
+             text-align: center;",
+            "Universidade Federal de Viçosa - UFV"),
+          
+          #Usado para criar um espaço entre o Título e a descrição
+          br(),
+          
+          #Link com um determinado nome
+          a("Currículo Lattes", href= "http://lattes.cnpq.br/6405218841067158",
+            style="font-family: 'Times New Roman'; font-size: 15pt; 
+             text-align: center;")
+         ),
+        
+        #Usado para criar um espaço entre o Título e a descrição
+        br(),
+        
+        
+        
+        #--------------
+        #4º box do desenvolvedor Zarzar
+        box(
+          status = "primary",
+          solidHeader = TRUE,
+          width = 6,
+          tags$img(src="Carlos.jpg", class = "img-circle",
+                   width = "200px", height = "200px"),
+          
+          #Usado para centralizar o que estiver dentro do box
+          style = "text-align: center",
+          
+          #Título com o nome da desenvolvedora
+          h1(style="font-family: 'Times New Roman'; font-size: 20pt; 
+             text-align: center; font-weight: bold;",
+             "Carlos Antônio Zarzar"),
+          
+          #Usado para criar um espaço entre o Título e a descrição
+          br(),
+          
+          #Descrição
+          p(style= "font-family: 'Times New Roman'; font-size: 15pt; 
+             text-align: center;",
+            "Universidade federal do Oeste do Pará - UFOPA Campus Monte Alegre"),
+          
+          #Usado para criar um espaço entre o Título e a descrição
+          br(),
+          
+          #Link com um determinado nome
+          a("Currículo Lattes", href= "http://lattes.cnpq.br/6505656674643382",
+            style="font-family: 'Times New Roman'; font-size: 15pt; 
+             text-align: center;")
+        ),
+        
+        
+        
+        #--------------
+        #5º Box do desenvolvedor Gabriel
+        box(
+          status = "primary",
+          solidHeader = TRUE,
+          width = 6,
+          tags$img(src="gabriel.png", class = "img-circle",
+                   width = "200px", height = "200px"),
+          
+            #Usado para centralizar o que estiver dentro do box
+          style = "text-align: center",
+          
+            #Título com o nome da desenvolvedora
+          h1(style="font-family: 'Times New Roman'; font-size: 20pt; 
+             text-align: center; font-weight: bold;",
+               "Gabriel Rodrigues da Silva"),
+          
+            #Usado para criar um espaço entre o Título e a descrição
+          br(),
+          
+          #Descrição
+          p(style= "font-family: 'Times New Roman'; font-size: 15pt; 
+             text-align: center;",
+            "Universidade federal do Oeste do Pará - UFOPA Campus Monte Alegre"),
+          
+          #Usado para criar um espaço entre o Título e a descrição
+          br(),
+          
+          #Link com um determinado nome
+          a("Currículo Lattes", href= "lattes.cnpq.br/8677409423160124",
+            style="font-family: 'Times New Roman'; font-size: 15pt; 
+             text-align: center;")
+        ),
+        
+        
+        
+        #--------------
+        #6º Box do desenvolvedor Luan
+        box(
+          status = "primary",
+          solidHeader = TRUE,
+          width = 6,
+          tags$img(src="luan.png", class = "img-circle",
+                   width = "200px", height = "200px"),
+          
+          #Usado para centralizar o que estiver dentro do box
+          style = "text-align: center",
+          
+          #Título com o nome da desenvolvedora
+          h1(style="font-family: 'Times New Roman'; font-size: 20pt; 
+             text-align: center; font-weight: bold;",
+             "Luan Patrick Moura de Souza"),
+          
+          #Usado para criar um espaço entre o Título e a descrição
+          br(),
+          
+          #Descrição
+          p(style= "font-family: 'Times New Roman'; font-size: 15pt; 
+             text-align: center;",
+            "Universidade federal do Oeste do Pará - UFOPA Campus Monte Alegre"),
+          
+          #Usado para criar um espaço entre o Título e a descrição
+          br(),
+          
+          #Link com um determinado nome
+          a("Currículo Lattes", href= "http://lattes.cnpq.br/8643319250252707",
+            style="font-family: 'Times New Roman'; font-size: 15pt; 
+             text-align: center;")
+        ),
+        
+        
+        
+        #--------------
+        #7º Box do desenvolvedor Daniel
+        box(
+          status = "primary",
+          solidHeader = TRUE,
+          width = 6,
+          tags$img(src="daniel.png", class = "img-circle",
+                   width = "200px", height = "200px"),
+          
+          #Usado para centralizar o que estiver dentro do box
+          style = "text-align: center",
+          
+          #Título com o nome da desenvolvedora
+          h1(style="font-family: 'Times New Roman'; font-size: 20pt; 
+             text-align: center; font-weight: bold;",
+             "Daniel Pinto Siqueira"),
+          
+          #Usado para criar um espaço entre o Título e a descrição
+          br(),
+          
+          #Descrição
+          p(style= "font-family: 'Times New Roman'; font-size: 15pt; 
+             text-align: center;",
+            "Universidade federal do Oeste do Pará - UFOPA Campus Monte Alegre"),
+          
+          #Usado para criar um espaço entre o Título e a descrição
+          br(),
+          
+          #Link com um determinado nome
+          a("Currículo Lattes", href= "http://lattes.cnpq.br/3311133937308698",
+            style="font-family: 'Times New Roman'; font-size: 15pt; 
+             text-align: center;")
+        ),
+        
+        
+        
+        #-------------
+        #Imagens de apoiadores
+        box(
+          width = 6,
+          status = "primary",
+            solidHeader = TRUE,
+          h1("Apoio", style= "font-family: 'Times New Roman';
+           font-size: 40pt; text-align: center;"),
+        
+            #logo do Curso
+          tags$img(src="logo_curso", class = "img-circle",
+                   width = "280px", height = "280px"),
+            #logo do Campus
+          tags$img(src="logo_campus", class = "img-circle",
+                   width = "280px", height = "280px"),
+          style= "text-align: center;"
+          )
+       )
+     )
+   )
+ )
 )
 
+
 server <- function(input, output) {
-  # Lendo os arquivos do upload
-  uploaded_data <- reactive({
-    req(input$file) # REQUERIDO Garante que o usuário tenha carregado um arquivo
-    read.csv(input$file$datapath) # Lê o arquivo CSV carregado pelo usuário
-  })
   
 }
 
 shinyApp(ui, server)
+
+
+
+
+
+
+
