931e8df580e700a909c5041541e146ecb146d4d6
rodriguesgabriel1 <gaabrielrodrigues12@gmail.com>
2023-11-13 08:55:21 -0300

    Interface da aba Contato

--- app.R
+++ app.R
@@ -1,51 +1,39 @@
-## app.R ##
 library(shiny)
 library(shinydashboard)
 
 ui <- dashboardPage(
-  # Cabeçalho
   dashboardHeader(
     title = "AppCovid Check"
   ),
-  # Barra lateral
   dashboardSidebar(
     sidebarMenu(
       menuItem("Home", tabName = "home"),
       menuItem("Help", tabName = "help"),
       menuItem("Desenvolvedores", tabName = "Dev"),
-      menuItem("Contato", tabName = "Contact")
+      menuItem("Contato", tabName = "contact")  # Corrigido para "contact"
     )
   ),
-  # Corpo do App
   dashboardBody(
-    #Colocar vários menus 
     tabItems(
-      #Primeiro menu HOME
       tabItem(
-        #Nome do menu desejado
         tabName = "home",
-          # 1 Box
-          box( # importando arquivo .fasta
-            fileInput("file", "Escolha um arquivo .fasta:",
-                      multiple = FALSE,
-                      accept = c(".fasta"))
-          )
-          #-----------
-          
-        ),
-      # Quarto menu Contato
-      tabItem(  
-        tabName = "Contact",
-        box("Contato",
-                 textInput("nome", "Nome:"),
-                 textInput("email", "Email:"),
-                 textAreaInput("mensagem", "Mensagem:"),
-                 submitButton("Enviar"),
-                 textOutput("feedback")
+        box(
+          fileInput("file", "Escolha um arquivo .fasta:",
+                    multiple = FALSE,
+                    accept = c(".fasta"))
         )
-        
+      ),
+      tabItem(
+        tabName = "contact",
+        box(
+        textInput("nome", "Nome:"),
+        textInput("email", "E-mail:"),
+        textAreaInput("mensagem", "Mensagem:"),
+        actionButton("enviar", "Enviar"),
+        textOutput("feedback")
+        )
       )
-      )
+    )
   )
 )
 
@@ -55,22 +43,16 @@ server <- function(input, output) {
     req(input$file) # REQUERIDO Garante que o usuário tenha carregado um arquivo
     read.csv(input$file$datapath) # Lê o arquivo CSV carregado pelo usuário
   })
-  ## crie essa função para dar funcionalidade ##
-  ## ao botão enviar mas, não funcionou!     ##
-  observeEvent(input$submit, {
-    # Execute ação quando o botão "Enviar" for clicado
-    nome <- input$nome
-    email <- input$email
-    mensagem <- input$mensagem
+  
+  # função observeEvent para lidar com o envio do formulário
+  observeEvent(input$enviar, {
+    # adicionar função para receber as mensagens
+    # você pode usar os valores de input$nome, input$email e input$mensagem
     
-    # Lógica para enviar um email, armazenar em um banco de dados, etc.
-    
-    # Atualize o feedback para o usuário
     output$feedback <- renderText({
-      paste("Obrigado por entrar em contato, ", nome, "!")
+      "Mensagem enviada com sucesso!"
     })
   })
-  
 }
 
 shinyApp(ui, server)
